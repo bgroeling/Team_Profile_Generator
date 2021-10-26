64cc6d7399d2362ee36ad099f635d912eff0a92d
bgroeling <84939419+bgroeling@users.noreply.github.com>
2021-10-26 01:58:25 -0400

    Manager Question prompt and conversion into html document

--- index.js
+++ index.js
@@ -10,16 +10,61 @@ const generateMarkdown = require('./generateMarkdown')
 const questions = [
     {
         type: 'input',
-        name: 'title',
-        message: 'What is the project Title?',
+        name: 'Name',
+        message: 'What is the managers name?',
         validate: nameInput => {
           if (nameInput) {
             return true;
           } else {
-            console.log('Please enter project Name!');
+            console.log('Please enter managers name!');
             return false;
           }
         }
+    },
+    {
+        type: 'input',
+        name: 'ID',
+        message: 'What is the employee ID',
+        validate: IDInput => {
+          if (IDInput) {
+            return true;
+          } else {
+            console.log('Please enter the ID!');
+            return false;
+          }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is their email?',
+        validate: EmailInput => {
+          if (EmailInput) {
+            return true;
+          } else {
+            console.log('Please enter their email!');
+            return false;
+          }
+        }
+    },
+    {
+        type: 'input',
+        name: 'OfficeNumber',
+        message: 'What is their office number?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter their office number!');
+            return false;
+          }
+        }
+    },
+];
+
+const teamQuestions = [
+    {
+       
     }
 ];
 
@@ -29,7 +74,14 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
+    // Manager Information
     inquirer.prompt(questions) 
+        .then(data => {
+        console.log(data)
+        writeToFile("index.html",generateMarkdown(data))
+    }),
+    // Rest of Team's Information
+    inquirer.prompt(teamQuestions) 
         .then(data => {
         console.log(data)
         writeToFile("index.html",generateMarkdown(data))
